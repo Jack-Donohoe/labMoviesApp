00b2bc149b8b585005039df3375de085c38e1113
Jack Donohoe <72226393+Jack-Donohoe@users.noreply.github.com>
2022-11-02 22:56:21 +0000

    Fix upcoming movies page.

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -73,10 +73,19 @@ export const getMovie = (args) => {
       });
   };
 
-  export const getUpcomingMovies = () => {
+  export const getUpcomingMovies = (args) => {
+    // console.log(args)
+    const [, idPart] = args.queryKey;
+    const { id } = idPart;
     return fetch(
-      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
-    )
-      .then(res => res.json())
-      .then(json => json.results);
+      `https://api.themoviedb.org/3/movie/upcoming/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error
+   });
   };
